15778cf4231680ca1a7f9b44a79c14b1454e0b86
shubham <109946001+Shubhamcha1@users.noreply.github.com>
2022-10-10 16:20:35 +0530

    heroku gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,13 @@ Rails.application.configure do
 
    config.assets.compile = true
 
+
+
+   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
 end
