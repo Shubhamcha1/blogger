6ab242de04c2889248918191ff1abb1a3f449f2a
shubham <109946001+Shubhamcha1@users.noreply.github.com>
2022-10-10 19:06:34 +0530

    procfile development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
    config.action_cable.allowed_request_origins = ['https://popblogger.herokuapp.com', 'http://popblogger.herokuapp.com']
    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+
+   
 if ENV["RAILS_LOG_TO_STDOUT"].present?
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
